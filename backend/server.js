3d0c70c682c00f66e92febf450dcd4f71b63970c
asaddev22 <asaddev22@gmail.com>
2024-09-28 21:55:25 +0100

    sdfdfdsfsd

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { getMarsPhotos, getAPOD, searchImages } = require('./controllers/nasaController'); 
 require('dotenv').config(); 
+const path = require('path'); // Add this line
 
 const app = express();
 const PORT = process.env.PORT || 5000;

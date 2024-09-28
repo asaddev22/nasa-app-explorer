2ed20c1f740cd24b4b5ecfbabc8e6bd008a89adb
asaddev22 <asaddev22@gmail.com>
2024-09-28 21:51:21 +0100

    how?

--- backend/server.js
+++ backend/server.js
@@ -10,11 +10,11 @@ app.use(cors({
   origin: 'https://nasa-web-explorer.onrender.com',  
 }));
 
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
 
-app.get('/', (req, res) => {
-  res.send('Welcome to NASA App Explorer Backend');
-});
+
+
 // Route to get Mars rover photos
 app.get('/mars', getMarsPhotos);
 
@@ -23,6 +23,13 @@ app.get('/apod', getAPOD);
 
 // Route to search NASA Image and Video Library
 app.get('/search', searchImages);
+
+//for redirects
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

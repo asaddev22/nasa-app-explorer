08c99d3b3f60e5f219429daa1e79503fb01455c0
asaddev22 <asaddev22@gmail.com>
2024-09-28 23:06:43 +0100

    okay

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const PORT = process.env.PORT || 5000;
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'https://nasa-web-explorer.onrender.com',  
+  origin: ['https://nasa-web-explorer.onrender.com', 'http://localhost:3000']  // Array of allowed origins
 }));
+
 
 
 
